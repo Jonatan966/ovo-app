ff02e15f739d2a2c8495dd2bb68cdae79c476a72
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-11-20 21:23:45 -0300

    feat: install and config styling lib

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,12 @@
+import { getCssText } from "../configs/style";
+
 export default function Head() {
   return (
-    <>
-      <title>Home page</title>
+    <head>
+      <title>ovo</title>
+      <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
-    </>
+    </head>
   );
 }
